eab06f7e3b5134512f62f6e7dca9f5845831a9b3
Will Verland <will@ver.land>
2024-05-30 08:37:50 +1200

    Show only path in shorten result

--- src/components/ShortUrlResult.jsx
+++ src/components/ShortUrlResult.jsx
@@ -4,7 +4,7 @@ export default function ShortUrlResult({ shortenedUrl, resetForm }) {
   return (
     <>
       <h2 className="text-center">
-        Your short URL is <a href={shortenedUrl}>{shortenedUrl}</a>
+        Your short URL is <a href={shortenedUrl}>{new URL(shortenedUrl).pathname}</a>
       </h2>
       <Button onClick={resetForm} variant="link" className="mt-3">
         ...shorten another URL
