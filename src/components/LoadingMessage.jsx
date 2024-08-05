348705338b131ee6e1d949cae661bba08a036791
Will Verland <will@ver.land>
2024-08-06 07:56:28 +1200

    Only show spinner after 200ms

--- src/components/LoadingMessage.jsx
+++ src/components/LoadingMessage.jsx
@@ -1,9 +1,16 @@
 import Spinner from 'react-bootstrap/Spinner';
 import PropTypes from 'prop-types';
+import { useEffect, useState } from 'react';
 
 
 const LoadingMessage = ({ message }) => {
-  return (
+  const [showMessage, setShowMessage] = useState(false);
+
+  useEffect(() => {
+    setTimeout(() => setShowMessage(true), 200);
+  }, []);
+
+  return showMessage && (
     <div className="flex-grow-1 d-flex justify-content-center align-items-center">
       <Spinner className="m-2" />
       <h3>{message}</h3>
